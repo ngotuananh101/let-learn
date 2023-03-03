caf574d661c6101c9e802cdb34d1804130e937d8
Ngo Tuan Anh <ngotuananh2101@gmail.com>
2023-03-03 22:32:07 +0700

    done manage user

--- database/migrations/2023_03_03_060101_create_schools_table.php
+++ database/migrations/2023_03_03_060101_create_schools_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('class_set', function (Blueprint $table) {
+        Schema::create('schools', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('class_id')->constrained('classes')->onDelete('cascade');
-            $table->foreignId('set_id')->constrained('sets')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('class_set');
+        Schema::dropIfExists('schools');
     }
 };
