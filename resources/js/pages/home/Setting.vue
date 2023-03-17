e114a1cd57d4827c15701fe7c486d784c0abea0b
hailongvu <92045866+hailongvu@users.noreply.github.com>
2023-03-18 00:02:17 +0700

    Done UI Setting and FlashCard

--- resources/js/pages/home/Setting.vue
+++ resources/js/pages/home/Setting.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="row mt-3">
         <h2 style="display: flex; justify-content: center; margin-bottom: 50px">Setting</h2>
-        <div class="col-2">
-            <i class="fa-solid fa-envelope" style="font-size: 100px;"></i>
+        <div class="col-2 d-flex justify-content-end align-items-center">
+            <i class="fa-solid fa-envelope fs-1"></i>
         </div>
         <div class="col-10">
             <div class="card">
@@ -10,12 +10,14 @@
                 <div class="card-body">
                     <div class="form-group">
                         <label for="new-email">New Email Address</label>
-                        <input type="text" id="new-email" name="new-email" class="form-control" placeholder="Enter new email">
+                        <input type="text" id="new-email" name="new-email" class="form-control"
+                               placeholder="Enter new email">
                     </div>
 
                     <div class="form-group">
                         <label for="learn-password">Learn Password</label>
-                        <input type="password" id="learn-password" name="learn-password" class="form-control" placeholder="Enter Learn password">
+                        <input type="password" id="learn-password" name="learn-password" class="form-control"
+                               placeholder="Enter Learn password">
                     </div>
 
                     <button type="submit" class="btn btn-primary">Submit</button>
@@ -24,16 +26,60 @@
         </div>
     </div>
     <div class="row mt-5">
-        <div class="col-2">
-            <i class="fa-solid fa-eye" style="font-size: 95px;"></i>
+        <div class="col-2 d-flex justify-content-end align-items-center">
+            <i class="fa-solid fa-lock fs-1" ></i>
         </div>
         <div class="col-10">
             <div class="card">
-                <h5 style="padding-left: 10px; padding-top: 10px;">Permanently delete Account</h5>
+                <h5 style="padding-left: 10px; padding-top: 10px;">Change PassWord</h5>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="new-email">Current PassWord</label>
+                        <input type="text" id="new-email" name="new-email" class="form-control"
+                               placeholder="Enter password">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="learn-password">New Password</label>
+                        <input type="password" id="learn-password" name="learn-password" class="form-control"
+                               placeholder="Enter new password">
+                    </div>
+                    <div class="form-group">
+                        <label for="learn-password">Re-New Password</label>
+                        <input type="password" id="learn-password" name="learn-password" class="form-control"
+                               placeholder="Enter re-new password">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-2 d-flex justify-content-end align-items-center">
+            <i class="fa-solid fa-moon-cloud fs-1"></i>
+        </div>
+        <div class="col-10">
+            <div class="card">
+                <h5 style="padding-left: 10px; padding-top: 10px;">NightMode</h5>
+                <div class="form-check form-switch" style="margin-left: 10px; margin-top: 20px">
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                    <label class="form-check-label" for="flexSwitchCheckDefault"></label>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-2 d-flex justify-content-end align-items-center">
+            <i class="fa-solid fa-eye fs-1"></i>
+        </div>
+        <div class="col-10">
+            <div class="card">
+                <h5 style="padding-left: 10px; padding-top: 10px;">How you appear on Quizlet</h5>
                 <div class="form-check" style="margin-left: 10px">
-                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" >
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
                     <label class="form-check-label" for="defaultCheck1">
-                       Show your real name on Let Learn
+                        Show your real name on Let Learn
                     </label>
                 </div>
                 <div class="form-check" style="margin-left: 10px">
@@ -41,7 +87,9 @@
                     <label class="form-check-label" for="defaultCheck2">
                         Show your profile on Let Learn
                     </label>
-                    <p>Removing a page from Google takes a variable amount of time depending on how recently Google indexed the page. If you have an urgent need to remove your profile from Google search, please contact us and we can do it for you manually</p>
+                    <p>Removing a page from Google takes a variable amount of time depending on how recently Google
+                        indexed the page. If you have an urgent need to remove your profile from Google search, please
+                        contact us and we can do it for you manually</p>
 
                 </div>
                 <div class="form-check">
@@ -51,8 +99,8 @@
         </div>
     </div>
     <div class="row mt-5">
-        <div class="col-2">
-            <i class="fa-solid fa-x" style="font-size: 110px;"></i>
+        <div class="col-2 d-flex justify-content-end align-items-center">
+            <i class="fa-solid fa-x fs-1"></i>
         </div>
         <div class="col-10">
             <div class="card">
--- resources/js/pages/learn/Flashcards.vue
+++ resources/js/pages/learn/Flashcards.vue
@@ -6,13 +6,8 @@
         <button @click="toggleSettings" class="settings-button">Settings</button>
 
         <section class="card-container">
-            <div class="card">
-                <div class="front">
-                    <p>{{ cards[currentCardIndex].front }}</p>
-                </div>
-                <div class="back">
-                    <p>{{ cards[currentCardIndex].back }}</p>
-                </div>
+            <div class="card d-flex justify-content-center align-items-center p-5" @click="rotateCard">
+                <p id="card-title" class="fs-3"></p>
             </div>
         </section>
         <div class="buttons-container">
@@ -20,22 +15,32 @@
             <button @click="nextCard">Next</button>
         </div>
         <!-- add settings menu popup -->
-        <div v-if="showSettings" class="settings-overlay">
-            <div class="settings-menu">
-                <!-- add settings button -->
-                <button class="settings-close-button" @click="toggleSettings">×</button>
-                <h2>Tùy chọn</h2>
-                <h6>Sắp xếp thẻ</h6>
-                <p>Sắp xếp thẻ của bạn để tập trung vào những thuật ngữ cần chú tâm học. Tắt tính năng sắp xếp nếu bạn muốn nhanh chóng ôn lại các thẻ ghi nhớ.</p>
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-                    <label class="form-check-label" for="flexSwitchCheckDefault">Âm thanh</label>
+
+        <div class="buttons">
+            <div v-if="showSettings" class="settings-overlay">
+                <div class="settings-menu">
+                    <!-- add settings button -->
+                    <button class="settings-close-button" @click="toggleSettings">×</button>
+                    <h2>Tùy chọn</h2>
+                    <h6>Sắp xếp thẻ</h6>
+                    <p>Sắp xếp thẻ của bạn để tập trung vào những thuật ngữ cần chú tâm học. Tắt tính năng sắp xếp nếu
+                        bạn muốn nhanh chóng ôn lại các thẻ ghi nhớ.</p>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                        <label class="form-check-label" for="flexSwitchCheckDefault">Âm thanh</label>
+                    </div>
+                    <!-- add your settings options here -->
+                    <button @click="toggleSettings">Close</button>
                 </div>
-                <!-- add your settings options here -->
-                <button @click="toggleSettings">Close</button>
+            </div>
+            <div>
+                <button id="quit-button" onclick="window.location.href = '/'">Quit</button>
+                <button id="announcement-button" @click="speak(cards[currentCardIndex].front)"><i
+                    class="fa-solid fa-bullhorn"></i></button>
             </div>
         </div>
     </div>
+
 </template>
 
 
@@ -74,8 +79,12 @@ export default {
                 }
             ],
             currentCardIndex: 0,
+            currentSide: 'front',
             showSettings: false // add showSettings data property
         };
+    },
+    mounted() {
+        document.getElementById('card-title').innerHTML = this.cards[this.currentCardIndex].front;
     },
     methods: {
         nextCard() {
@@ -84,6 +93,7 @@ export default {
             } else {
                 this.currentCardIndex = 0;
             }
+            document.getElementById('card-title').innerHTML = this.cards[this.currentCardIndex].front;
         },
         previousCard() {
             if (this.currentCardIndex > 0) {
@@ -91,9 +101,30 @@ export default {
             } else {
                 this.currentCardIndex = this.cards.length - 1;
             }
+            document.getElementById('card-title').innerHTML = this.cards[this.currentCardIndex].front;
         },
         toggleSettings() { // add toggleSettings method
             this.showSettings = !this.showSettings;
+        },
+        // text to speech
+        speak(text) {
+            const msg = new SpeechSynthesisUtterance();
+            msg.text = text;
+            window.speechSynthesis.speak(msg);
+        },
+        rotateCard(e) {
+            let card = e.target.closest('.card');
+            if (this.currentSide === 'front') {
+                card.classList.add('rotate');
+                document.getElementById('card-title').classList.add('card-title-transition');
+                document.getElementById('card-title').innerHTML = this.cards[this.currentCardIndex].back;
+                this.currentSide = 'back';
+            } else {
+                card.classList.remove('rotate');
+                document.getElementById('card-title').classList.remove('card-title-transition');
+                document.getElementById('card-title').innerHTML = this.cards[this.currentCardIndex].front;
+                this.currentSide = 'front';
+            }
         }
     },
     computed: {
@@ -108,15 +139,23 @@ export default {
 </script>
 
 
-
-
 <style scoped>
+
+.rotate {
+    transform: rotateY(180deg);
+}
+
+.card-title-transition {
+    transform: rotateY(180deg);
+}
+
 .card-count {
     position: absolute;
     top: 10px;
     left: 10px;
     font-size: 18px;
 }
+
 .settings-button {
     position: absolute;
     top: 10px;
@@ -209,14 +248,6 @@ export default {
     padding-bottom: 50px; /* add bottom padding */
 }
 
-.card-container:hover .front {
-    transform: rotateY(-180deg);
-}
-
-.card-container:hover .back {
-    transform: rotateY(0deg);
-}
-
 .card {
     position: absolute;
     top: 0;
@@ -226,46 +257,6 @@ export default {
     transition: all 0.7s;
     transform-style: preserve-3d;
     background: transparent;
-}
-.front p,
-.back p {
-    font-size: 24px;
-}
-.front {
-    background-color: #00cc33; /* Set green background color */
-    color: #ffffff;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    z-index: 2; /* bring front card to front */
-    transform: rotateY(0deg);
-    transition: all 0.7s;
-    backface-visibility: hidden;
-}
-
-.back {
-    background-color: #0080ff; /* Set blue background color */
-    color: #ffffff;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    z-index: 1; /* send back card to back */
-    transform: rotateY(-180deg);
-    transition: all 0.7s;
-    backface-visibility: hidden;
-}
-
-.front,
-.back {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-weight: bold;
-    border-radius: 4px;
-    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 }
 
 .buttons-container {
@@ -283,5 +274,32 @@ button {
     background-color: #f5f5f5;
     border: none;
     cursor: pointer;
+}
+
+.buttons {
+    position: absolute;
+    bottom: 10px; /* set bottom offset */
+    right: 10px; /* set right offset */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 999; /* make sure it appears above other elements */
+}
+
+#announcement-button {
+    margin-bottom: 400px;
+    margin-left: 10px;
+    padding: 10px;
+    border-radius: 4px;
+    background-color: #f5f5f5;
+    border: none;
+    cursor: pointer;
+    position: absolute; /* add position: absolute */
+    bottom: 10px; /* set bottom offset */
+    right: 90px; /* adjust right offset as needed */
+}
+
+.fa-solid.fa-bullhorn {
+    font-size: 18px;
 }
 </style>
