26e3843f83ec1345fe8becc9b5c624f22fe39605
DESKTOP-J1C3NL0\quang <quangtuan06052000@gmail.com>
2023-03-20 22:32:35 +0700

    Done function show relearn + notlearn

--- resources/js/helpers/services/home.service.js
+++ resources/js/helpers/services/home.service.js
@@ -7,7 +7,9 @@ export const homeService = {
     loadLesson,
     loadCourse,
     getLessonByCourseId,
-    showLessonDetailOfRelearnAndNotYetLearn
+    showLessonDetailRelearn,
+    showLessonDetailNotLearn,
+    getLessonInfo
 }
 
 function loadLesson() {
@@ -39,18 +41,40 @@ function getLessonByCourseId(course_id) {
         });
 }
 
-function showLessonDetailOfRelearnAndNotYetLearn(lesson_id) {
+//show lesson detail of relearn
+function showLessonDetailRelearn(lesson_id){
+    console.log(lesson_id);
     let user = JSON.parse(localStorage.getItem('user'));
     let user_id = user.id;
-    return fetch(`${config.apiUrl}/user/${user_id}?type=detail_split&lesson_id=${lesson_id}`, {
-        method: 'GET',
-        headers: authHeader()
-    })
+    return fetch(`${config.apiUrl}/user/${user_id}?type=detail_split&lesson_id=${lesson_id}`, { method: 'GET', headers: authHeader() })
         .then(handleResponse)
         .then(data => {
-                return data.data;
-            }
-        );
+            return data.data.relearn;
+        }
+    );
+}
+
+//show lesson detail of not learn
+function showLessonDetailNotLearn(lesson_id){
+    console.log(lesson_id);
+    let user = JSON.parse(localStorage.getItem('user'));
+    let user_id = user.id;
+    return fetch(`${config.apiUrl}/user/${user_id}?type=detail_split&lesson_id=${lesson_id}`, { method: 'GET', headers: authHeader() })
+        .then(handleResponse)
+        .then(data => {
+            return data.data.notLearn;
+        }
+    );
+}
+
+//show lesson info
+function getLessonInfo(lesson_id) {
+    console.log(lesson_id);
+    return fetch(`${config.apiUrl}/lesson/${lesson_id}`, {method: 'GET', headers: authHeader()})
+        .then(handleResponse)
+        .then(data => {
+            return data.data.lesson;
+        });
 }
 
 
