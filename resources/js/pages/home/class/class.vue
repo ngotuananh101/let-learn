0334170db49e70ac4ec89fb451c24a26ecf9a763
hailongvu <92045866+hailongvu@users.noreply.github.com>
2023-03-23 02:32:27 +0700

    Fix result of learn and Done UI class

---
+++ resources/js/pages/home/class/class.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container-fluid">
+        <div class="row justify-content-center align-items-center pt-4 w-100">
+            <div class="col-md-12">
+                <div class="card text-white bg-dark mb-3">
+                    <img src="https://example.com/image.png" class="card-img" alt="IS1431">
+                    <div class="card-img-overlay d-flex flex-column justify-content-center">
+                        <h5 class="card-title">Learning Title</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    //height: 120%;
+    width: 100%;
+    height: 250px;
+    padding-top: 10%;
+    padding-left: 10%;
+    padding-right: 10%;
+    background: #3aaf85;
+}
+</style>
