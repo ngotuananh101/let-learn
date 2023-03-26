7866a528d0e093201f4a74bbd314322f0cdd039d
hailongvu <92045866+hailongvu@users.noreply.github.com>
2023-03-26 23:12:44 +0700

    Done UI Class

--- resources/js/pages/home/class/Class.vue
+++ resources/js/pages/home/class/Class.vue
@@ -126,8 +126,63 @@
                                             <button class="btn btn-primary">Start</button>
                                         </div>
                                         <div class="col-2">
-                                            <button class="btn btn-primary" @click="viewExerciseResult(lesson.id)">Result</button>
+<!--                                            <button class="btn btn-primary" @click="viewExerciseResult(lesson.id)">Result</button>-->
+                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                Result
+                                            </button>
+                                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog modal-fullscreen">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="exampleModalLabel">Detailed test results of students</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <div class="pb-0 card-header">
+                                                                <div class="d-lg-flex">
+                                                                    <div>
+                                                                        <h5 class="mb-0">Java - Lesson 1</h5>
+                                                                        <p class="mb-0 text-sm">List all result of test</p>
+                                                                    </div>
+                                                                    <div class="my-auto mt-4 ms-auto mt-lg-0">
+                                                                        <div class="my-auto ms-auto">
+                                                                            <button type="button" class="mx-1 mb-0 btn btn-outline-success btn-sm">Export to exel</button>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="px-0 pb-0 pt-0 card-body">
+                                                                <div class="table-responsive">
+                                                                    <DataTable id="setdata" :options="{select: 'single'}" ref="table" class="table table-flush mx-3">
+                                                                        <thead class="thead-light">
+                                                                        <tr>
+                                                                            <th>ID</th>
+                                                                            <th>Name</th>
+                                                                            <th>Score</th>
+                                                                            <th>Time to finish</th>
+                                                                            <th>Status</th>
+                                                                        </tr>
+                                                                        </thead>
+                                                                        <tbody>
+                                                                        <tr v-for="(result, index) in results" :key="index">
+                                                                            <td>{{ result.id }}</td>
+                                                                            <td>{{ result.name }}</td>
+                                                                            <td>{{ result.score }}</td>
+                                                                            <td>{{ result.time }}</td>
+                                                                            <td>{{ result.status }}</td>
+                                                                        </tr>
+                                                                        </tbody>
+                                                                    </DataTable>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
 
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -210,8 +265,15 @@
 </template>
 
 <script>
+import DataTable from 'datatables.net-vue3'
+import DataTablesLib from 'datatables.net-bs5';
+import 'datatables.net-select';
 
+DataTable.use(DataTablesLib);
 export default {
+    components:{
+        DataTable,
+    },
     data() {
         return {
             lessons: [
@@ -234,6 +296,22 @@ export default {
                     showDetails: false,
                 },
             ],
+            table: null,
+            results: [
+                { id: 1, name: "Vu Hai Long", score: "80", time: "15:34", status: "submitted" },
+                { id: 2, name: "Ngo Tuan Anh", score: "89", time: "15:38", status: "unsubmitted" },
+                { id: 3, name: "Dao Xuan Dat", score: "87", time: "15:34", status: "submitted" },
+                { id: 4, name: "Nguyen Quang Tuan", score: "45", time: "35:34", status: "unsubmitted" },
+                { id: 5, name: "Tran Manh Dat", score: "25", time: "15:34", status: "submitted" },
+                { id: 6, name: "Pham Thi An", score: "95", time: "16:22", status: "unsubmitted" },
+                { id: 7, name: "Nguyen Van A", score: "70", time: "14:56", status: "submitted" },
+                { id: 8, name: "Le Thi B", score: "82", time: "16:08", status: "unsubmitted" },
+                { id: 9, name: "Truong Duc C", score: "68", time: "15:12", status: "submitted" },
+                { id: 10, name: "Doan Minh D", score: "93", time: "17:04", status: "submitted" },
+                { id: 11, name: "Le Tran Gia Huy", score: "75", time: "15:50", status: "unsubmitted" },
+                { id: 12, name: "Phan Thi Kim Ngan", score: "78", time: "16:34", status: "submitted" }
+
+            ],
         };
     },
     mounted() {
@@ -242,6 +320,14 @@ export default {
             .catch(error => {
                 console.error(error);
             });
+        let delay = setInterval(() => {
+            if (this.table) {
+                this.table.clear();
+                this.table.rows.add(this.results);
+                this.table.draw();
+                clearInterval(delay);
+            }
+        }, 100);
     },
     methods: {
         onSubmit() {
