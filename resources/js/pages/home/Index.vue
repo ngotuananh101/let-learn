b2fb1fa34b4ec20315a492f9aff0086299558ede
DESKTOP-J1C3NL0\quang <quangtuan06052000@gmail.com>
2023-04-02 01:06:19 +0700

    Done modal add course, fix delete course

--- resources/js/pages/home/Index.vue
+++ resources/js/pages/home/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="my-4">
         <info-card title="Welcome to Let Learn" description="Learn anything, anytime, anywhere"
-                   :badge="{ text: 'Moderate', color: 'success' }" />
+            :badge="{ text: 'Moderate', color: 'success' }" />
     </div>
     <div class="mt-5">
         <div class="row">
@@ -9,7 +9,7 @@
             <div class="col-lg-4 col-md-6 col-12" v-for="lesson in lessons">
                 <router-link :to="`/lesson/${lesson.id}`">
                     <LessonCard :title="lesson.name" :value="lesson.detail_count + ' terms'"
-                                :description="lesson.username" />
+                        :description="lesson.username" />
                 </router-link>
             </div>
         </div>
@@ -18,7 +18,7 @@
             <div class="col-lg-4 col-md-6 col-12" v-for="course in courses">
                 <router-link :to="`/course/${course.id}`">
                     <LessonCard :title="course.name" :value="course.lesson_count + ' lessons'"
-                                :description="course.username" />
+                        :description="course.username" />
                 </router-link>
             </div>
         </div>
@@ -40,8 +40,8 @@
                     <div class="col-md-3 col-6 p-3">
                         <div class="card" @click="createLesson">
                             <div class="p-5 pb-4 m-4 mb-0">
-                                <img src="https://cdn-icons-png.flaticon.com/512/5484/5484383.png"
-                                     class="card-img-top" alt="...">
+                                <img src="https://cdn-icons-png.flaticon.com/512/5484/5484383.png" class="card-img-top"
+                                    alt="...">
                             </div>
                             <div class="card-body p-2 text-center text-black py-4">
                                 <h5 class="card-title">Create lesson</h5>
@@ -50,10 +50,10 @@
                         </div>
                     </div>
                     <div class="col-md-3 col-6 p-3">
-                        <div class="card">
+                        <div class="card" data-bs-toggle="modal" href="#modalCreateCourse">
                             <div class="p-5 pb-4 m-4 mb-0">
-                                <img src="https://cdn-icons-png.flaticon.com/512/7688/7688940.png"
-                                     class="card-img-top" alt="...">
+                                <img src="https://cdn-icons-png.flaticon.com/512/7688/7688940.png" class="card-img-top"
+                                    alt="...">
                             </div>
                             <div class="card-body p-2 text-center text-black py-4">
                                 <h5 class="card-title">Create course</h5>
@@ -64,8 +64,8 @@
                     <div class="col-md-3 col-6 p-3">
                         <div class="card">
                             <div class="p-5 pb-4 m-4 mb-0">
-                                <img src="https://cdn-icons-png.flaticon.com/512/10215/10215645.png"
-                                     class="card-img-top" alt="...">
+                                <img src="https://cdn-icons-png.flaticon.com/512/10215/10215645.png" class="card-img-top"
+                                    alt="...">
                             </div>
                             <div class="card-body p-2 text-center text-black py-4">
                                 <h5 class="card-title">Import lesson</h5>
@@ -76,8 +76,8 @@
                     <div class="col-md-3 col-6 p-3">
                         <div class="card">
                             <div class="p-5 pb-4 m-4 mb-0">
-                                <img src="https://cdn-icons-png.flaticon.com/512/8744/8744051.png"
-                                     class="card-img-top" alt="...">
+                                <img src="https://cdn-icons-png.flaticon.com/512/8744/8744051.png" class="card-img-top"
+                                    alt="...">
                             </div>
                             <div class="card-body p-2 text-center text-black py-4">
                                 <h5 class="card-title">Faqs</h5>
@@ -89,17 +89,43 @@
             </div>
         </div>
     </div>
+    <div class="modal" id="modalCreateCourse" data-bs-keyboard="false" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered modal-xl">
+            <div class="modal-content p-md-5">
+                <div class="modal-body">
+                    <div>
+                        <label class="form-label mt-2 fs-6">Name</label>
+                        <argon-input id="name" type="text" name="name"
+                            placeholder="Enter a name. Example:'MAE Chapter 1' " />
+                    </div>
+                    <div>
+                        <label class="form-label mt-2 fs-6">Description</label>
+                        <argon-input id="description" type="text" name="description" placeholder="Add a description..." />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-1">
+                        <argon-button color="default" variant="gradient" data-bs-toggle="modal" href="#modal">Cancel</argon-button>
+                    </div>
+                    <div class="col-2">
+                        <argon-button color="success" variant="gradient" @click="this.createCourse">Create</argon-button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import LessonCard from "@/components/Cards/LessonCard.vue";
 import InfoCard from "@/components/Cards/InfoCard.vue";
 import ArgonButton from "@/components/Argons/ArgonButton.vue";
-import {Modal} from "bootstrap";
+import ArgonInput from "@/components/Argons/ArgonInput.vue";
+import { Modal } from "bootstrap";
 
 export default {
     name: "HomePage",
-    components: { LessonCard, InfoCard, ArgonButton },
+    components: { LessonCard, InfoCard, ArgonButton, ArgonInput },
     data() {
         return {
             lessons: null,
@@ -134,6 +160,17 @@ export default {
             this.addModal.hide();
             // redirect to add lesson page
             this.$router.push({ name: "home.lesson.add" });
+        },
+        createCourse() {
+            let name = document.getElementById('name').value;
+            let description = document.getElementById('description').value;
+            if (name === '' || description === '') {
+                alert('Please fill all fields to create new course');
+            } else {
+                this.$store.dispatch('course/createCourse', { name: name, description: description}).then(() => {
+                    this.$router.push({ name: "home.index"});
+                });
+            }
         }
     }
 };
