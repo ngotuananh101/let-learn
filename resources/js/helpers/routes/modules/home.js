e3ed49b0f38d9d06042587c2804094f8df47ad8c
hailongvu <92045866+hailongvu@users.noreply.github.com>
2023-04-11 17:08:49 +0700

    Home

--- resources/js/helpers/routes/modules/home.js
+++ resources/js/helpers/routes/modules/home.js
@@ -1,12 +1,24 @@
 const home = [
     {
-        path: '',
+        path: '/',
         name: 'home',
-        component: () => import('../../../pages/home/home.vue'),
+        component: () => import('../../../layouts/home.vue'),
         meta: {
             requiresAuth: true,
-        }
-    }
+        },
+        children: [
+            {
+                path: '',
+                name: 'home.home',
+                component: () => import('../../../pages/home/home.vue'),
+            },
+            {
+                path: 'profile',
+                name: 'home.profile',
+                component: () => import('../../../pages/home/option/profile.vue'),
+            }
+    ],
+    },
 ];
 
 export default home;
--- resources/js/pages/home/home.vue
+++ resources/js/pages/home/home.vue
@@ -1,10 +1,91 @@
-<template lang="">
+<template>
+    <div class="my-4">
+        <div class="card">
+            <div class="card-body text-success text-sans-serif text-2xl">
+                Learn anything, anytime, anywhere
+            </div>
+        </div>
+    </div>
+    <div class="mt-5">
+        <div class="row">
+            <h3>Lesson</h3>
+            <div class="col-lg-4 col-md-6 col-12" v-for="lesson in lessons">
+                <router-link :to="`/lesson/${lesson.id}`">
+                    <div class="card mt-4">
+                        <div class="card-body">
+                            <p class="card-text text-primary">{{ lesson.name }}</p>
+                            <p class="card-text">Quantity: {{ lesson.detail_count }}</p>
+                            <p class="card-text">Author: {{ lesson.username }}</p>
+                        </div>
+                    </div>
+                </router-link>
+            </div>
+        </div>
+        <div class="row">
+            <h3>Course</h3>
+            <div class="col-lg-4 col-md-6 col-12" v-for="course in courses">
+                <router-link :to="`/course/${course.id}`">
+                    <LessonCard :title="course.name" :value="course.lesson_count + ' lessons'"
+                                :description="course.username"/>
+                </router-link>
+            </div>
+        </div>
+        <div v-if="classes" class="row">
+            <h2>Class</h2>
+            <div class="col-lg-4 col-md-6 col-12" v-for="classes in classes">
+                <LessonCard :title="classes.name" :value="classes.classes_count + ' courses'"/>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    
+    name: 'home',
+    data() {
+        return {
+            lessons: [],
+            courses: [],
+            classes: []
+        }
+    },
+    created() {
+        this.lessons = [
+            {
+                id: 1,
+                name: 'Lesson 1',
+                detail_count: 10,
+                username: 'admin'
+            },
+            {
+                id: 2,
+                name: 'Lesson 2',
+                detail_count: 10,
+                username: 'admin'
+            },
+            {
+                id: 3,
+                name: 'Lesson 3',
+                detail_count: 10,
+                username: 'admin'
+            },
+            {
+                id: 4,
+                name: 'Lesson 4',
+                detail_count: 10,
+                username: 'admin'
+            },
+            {
+                id: 5,
+                name: 'Lesson 5',
+                detail_count: 10,
+                username: 'admin'
+            },
+        ]
+    },
+
+
 }
 </script>
 <style lang="">
-    
+
 </style>
