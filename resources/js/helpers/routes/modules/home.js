f3209b76dcbda0e0a44b5028e85f08f63f97a3a6
DESKTOP-J1C3NL0\quang <quangtuan06052000@gmail.com>
2023-04-12 03:04:07 +0700

    Done floating button home

--- resources/js/helpers/routes/modules/home.js
+++ resources/js/helpers/routes/modules/home.js
@@ -6,7 +6,42 @@ const home = [
         meta: {
             requiresAuth: true,
         }
-    }
+    },
+    {
+        path: 'lesson',
+        children: [
+            {
+                path: ':id',
+                name: 'lesson.index',
+                component: () => import('../../../pages/lesson/Lesson.vue'),
+            },
+            // {
+            //     path: ':id/edit',
+            //     name: 'lesson.edit',
+            //     component: () => import('../../pages/lesson/Edit.vue'),
+            // },
+            // {
+            //     path: 'add',
+            //     name: 'lesson.add',
+            //     component: () => import('../../pages/lesson/Add.vue'),
+            // },
+        ],
+    },
+    // {
+    //     path: 'course',
+    //     children: [
+    //         {
+    //             path: ':id',
+    //             name: 'home.course.index',
+    //             component: () => import('../../pages/course/Course.vue'),
+    //         },
+    //         {
+    //             path: ':id/edit',
+    //             name: 'home.course.edit',
+    //             component: () => import('../../pages/course/Edit.vue'),
+    //         },
+    //     ],
+    // },
 ];
 
 export default home;
